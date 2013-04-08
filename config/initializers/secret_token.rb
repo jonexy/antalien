7cbdb3f20f7b64a7e5aa29e1dac223efa57fca67
Jonex Ybanez <jonex_ybanez@yahoo.com>
2013-04-09 00:41:25 +0800

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Antalien::Application.config.secret_token = '0b2f95958468bbbcec9d8bf06dc0f3d38639b261cd39f014543735e89906949a7757646ac0fdfc5bb7f2f1a4333b9245f640667bf285fabb642d54e8f9b37d38'
